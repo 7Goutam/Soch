a1d4d6fc28dc9932542f15e76f779eac4aea99c5
Goutam Jha <jhagoutam804@gmail.com>
2025-07-02 09:52:15 +0530

    Replace Impact with AboutUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import AboutUs from './Components/AboutUs/AboutUs'
+import AboutUs from './Components/Impact/Impact'
 import Footer from './Components/Footer/Footer'
 import HeroSection from './Components/Hero/Hero'
 import Gallery from './Components/Gallery/Gallery'
